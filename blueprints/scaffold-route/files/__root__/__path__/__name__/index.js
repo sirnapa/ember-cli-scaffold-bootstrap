490142581fa2db93a1792fa00f94582d7cf7a49f
Nelson Páez <nelpa90@gmail.com>
2016-11-02 17:49:22 -0300

    Mejoras en el scaffold y mensajes de internacionalización

--- blueprints/scaffold-route/files/__root__/__path__/__name__/index.js
+++ blueprints/scaffold-route/files/__root__/__path__/__name__/index.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  i18n: Ember.inject.service(),
+
   actions: {
     remove: function(model) {
-      if(confirm('Are you sure?')) {
+      if(confirm(this.get('i18n').t('action.confirm'))) {
         model.destroyRecord();
       }
     }
   },
+
   model: function() {
     return this.store.findAll('<%= dasherizedModuleName %>');
   }
