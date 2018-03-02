7ecb735a844c357ba42038ea0c8020c7b5418263
Nelson Paez <nelpa90@gmail.com>
2018-03-02 06:03:24 -0300

    Acciones en el show

--- blueprints/scaffold-route/files/__root__/__path__/__name__/show.js
+++ blueprints/scaffold-route/files/__root__/__path__/__name__/show.js
@@ -2,4 +2,25 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
+  i18n: Ember.inject.service(),
+  notify: Ember.inject.service('notify'),
+  busy: Ember.inject.service(),
+
+  actions: {
+    remove: function(model) {
+      var route = this;
+      route.get('busy').show();
+
+      if(confirm(this.get('i18n').t('action.confirm'))) {
+        model.destroyRecord().then(function(){
+          route.get('busy').hide();
+          route.transitionTo('<%= dasherizedModuleNamePlural %>');
+          route.get('notify').success(route.get('i18n').t('<%= dasherizedModuleName %>.messages.delete.success'));
+        }, function(){
+          route.get('busy').hide();
+          route.get('notify').error(route.get('i18n').t('<%= dasherizedModuleName %>.messages.delete.error'));
+        });
+      }
+    }
+  }
 });
