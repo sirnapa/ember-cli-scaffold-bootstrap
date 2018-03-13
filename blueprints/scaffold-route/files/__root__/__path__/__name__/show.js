a8b844d362767a6244e1b9876c44cf5bbd7715a3
Nelson Paez <nelpa90@gmail.com>
2018-03-13 10:56:03 -0300

    Botones de editar y remover dentro del show

--- blueprints/scaffold-route/files/__root__/__path__/__name__/show.js
+++ blueprints/scaffold-route/files/__root__/__path__/__name__/show.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
+  i18n: Ember.inject.service(),
+
+  actions: {
+    remove: function(model) {
+      if(confirm(this.get('i18n').t('action.confirm'))) {
+        model.destroyRecord();
+      }
+    }
+  }
 });
